0e52261ff8aaa1f5a9839bf344194977a5280b8f
erinrkahn <77405273+erinrkahn@users.noreply.github.com>
2021-03-14 16:25:29 -0700

    updated Employee_Database_challenge.sql with 2 additional querie

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -29,4 +29,45 @@ FROM unique_titles
 GROUP BY title
 ORDER BY (emp_count) DESC;
 
+-- Mentorship eligibility
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE ((e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') 
+	   AND (de.to_date = '9999-01-01'))
+ORDER BY (e.emp_no);
 
+-- Additional Queries
+
+-- 1) Count unique titles for mentorship_eligibility group
+SELECT title, COUNT(*) AS emp_count
+FROM mentorship_eligibility
+GROUP BY title
+ORDER BY (emp_count) DESC;
+
+-- 2) Identify individuals with mentorship eligibilty that have held titles the longest
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE ((e.birth_date BETWEEN '1965-01-01' AND '1965-12-31') 
+	   AND (de.to_date = '9999-01-01'))
+ORDER BY (e.emp_no), (de.from_date) ASC;
